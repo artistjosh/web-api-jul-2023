ae3e34980db6111f0c6b8dadd82a989e0b6bb47c
Allan <allanjoshmills@gmail.com>
2023-08-01 12:29:16 -0400

    halfofday2

--- EmployeesHrSolution/EmployeesHrAPI/Controllers/EmployeesController.cs
+++ EmployeesHrSolution/EmployeesHrAPI/Controllers/EmployeesController.cs
@@ -9,17 +9,45 @@ public class EmployeesController : ControllerBase
 {
 
     private readonly EmployeeDataContext _context;
+    private readonly ILogger<EmployeesController> _logger;
 
-    public EmployeesController(EmployeeDataContext context)
+    public EmployeesController(EmployeeDataContext context, ILogger<EmployeesController> logger)
     {
         _context = context;
+        _logger = logger;
+    }
+    [HttpGet("/employees/{employeeId:int}")]
+    public async Task<ActionResult> GetAnEmployeeAsync(int employeeId)
+    {
+        _logger.LogInformation("Got the following employeeId {0}", employeeId);
+        var employee = await _context.Employees
+            .Where(e => e.Id == employeeId)
+            .Select(e=> new EmployeeDetailsResponseModel
+            {
+                Id = e.Id.ToString(),
+                FirstName = e.FirstName,
+                LastName = e.LastName,
+                Department = e.Department,
+                Email = e.Email,
+                PhoneExtension = e.PhoneExtensions
+            })
+            .SingleOrDefaultAsync();
+
+        if (employee is null)
+        {
+            return NotFound(); // 404
+        }
+        else
+        {
+            return Ok(employee);
+        }
     }
 
     // GET /employees
     [HttpGet("/employees")]
-    public async Task<ActionResult> GetEmployeesAsync()
+    public async Task<ActionResult<EmployeesResponseModel>> GetEmployeesAsync([FromQuery] String department = "All") 
     {
-        var employees = await _context.Employees
+        var employees = await _context.GetEmployeesByDepartment(department)
             .Select(emp => new EmployeesSummaryResponseModel
             {
                 Id = emp.Id.ToString(),
@@ -28,11 +56,13 @@ public class EmployeesController : ControllerBase
                 Department = emp.Department,
                 Email = emp.Email,
             })
-            .ToListAsync();
+            .ToListAsync(); //runs the query
+
         // TODO: Get Back to This
         var response = new EmployeesResponseModel
         {
-            Employees = employees
+            Employees = employees,
+            ShowingDepartment = department
         };
         return Ok(response);
     }
